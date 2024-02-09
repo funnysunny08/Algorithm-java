c5db55aa4ca1cca380add9e4d8c32ed805fb5a39
sunny <rosa7536@naver.com>
2024-02-09 11:57:57 +0900

    Study) boj 13549

---
+++ src/boj/BOJ_13549.java
@@ -0,0 +1,62 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// 백준 13549번: 숨바꼭질 3
+public class BOJ_13549 {
+
+    static int N, K;
+    static int answer = Integer.MAX_VALUE;
+    static boolean[] visited;
+    static int MAX = 100000;
+
+    static class Node {
+
+        int x, time;
+
+        Node(int x, int time) {
+            this.x = x;
+            this.time = time;
+        }
+    }
+
+    public static int bfs() {
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(N, 0));
+
+        while (!q.isEmpty()) {
+            Node currNode = q.poll();
+            visited[currNode.x] = true;
+
+            if (currNode.x == K) {
+                answer = Math.min(answer, currNode.time);
+            }
+
+            if (currNode.x * 2 <= MAX && !visited[currNode.x * 2]) {
+                q.add(new Node(currNode.x * 2, currNode.time));
+            }
+            if (currNode.x + 1 <= MAX && !visited[currNode.x + 1]) {
+                q.add(new Node(currNode.x + 1, currNode.time + 1));
+            }
+            if (currNode.x - 1 >= 0 && !visited[currNode.x - 1]) {
+                q.add(new Node(currNode.x - 1, currNode.time + 1));
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        visited = new boolean[100001];
+
+        System.out.println(bfs());
+    }
+}
