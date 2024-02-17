426872a304984f8f8a5784427da41152f598391d
Sunny <rosa7536@naver.com>
2024-02-17 12:24:23 +0900

    Study) boj 17182

---
+++ src/boj/BOJ_17182.java
@@ -0,0 +1,58 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 백준 17182번: 우주 탐사선
+public class BOJ_17182 {
+
+    static int N, K; // 행성 개수, 시작 위치
+    static int[][] map;
+    static int answer = Integer.MAX_VALUE;
+    static boolean[] visited;
+
+    public static void dfs(int depth, int node, int sum) {
+        if (depth == N - 1) {
+            answer = Math.min(answer, sum);
+            return;
+        }
+        visited[node] = true;
+        for (int i = 0; i < N; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            dfs(depth + 1, i, sum + map[node][i]);
+            visited[i] = false;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        map = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) { // 경유 노드
+            for (int i = 0; i < N; i++) { // 시작 노드
+                for (int j = 0; j < N; j++) { // 도착 노드
+                    if (map[i][j] > map[i][k] + map[k][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
+                }
+            }
+        }
+
+        visited = new boolean[N];
+        dfs(0, K, 0);
+        System.out.println(answer);
+    }
+}
